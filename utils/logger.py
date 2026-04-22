6c1f0c87b05b5de685a2aabc8683bd4e2d3fbbcd
Youssefzdb <149632182+Youssefzdb@users.noreply.github.com>
2026-04-22 23:48:41 +0100

    feat: add utils/logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,9 @@
 class Logger:
-    def __init__(self, verbose=False):
-        self.verbose = verbose
-    def info(self, m):    print(f"\033[94m[*]\033[0m {m}")
-    def success(self, m): print(f"\033[92m[+]\033[0m {m}")
-    def warning(self, m): print(f"\033[93m[!]\033[0m {m}")
-    def error(self, m):   print(f"\033[91m[-]\033[0m {m}")
+    R="[91m"; G="[92m"; Y="[93m"; B="[94m"; X="[0m"
+    def __init__(self, verbose=False): self.verbose = verbose
+    def info(self, m):    print(f"{self.B}[*]{self.X} {m}")
+    def success(self, m): print(f"{self.G}[+]{self.X} {m}")
+    def warning(self, m): print(f"{self.Y}[!]{self.X} {m}")
+    def error(self, m):   print(f"{self.R}[-]{self.X} {m}")
     def debug(self, m):
-        if self.verbose: print(f"[D] {m}")
+        if self.verbose: print(f"[DBG] {m}")
